1646512b301768e7c8f89eadb509586dbdff9bbb
RuffTrigger <jeanmduus@gmail.com>
2024-07-06 10:58:14 +0200

    v1.0

--- src/main/java/org/rufftrigger/eternalharvest/Main.java
+++ src/main/java/org/rufftrigger/eternalharvest/Main.java
@@ -2,6 +2,8 @@ package org.rufftrigger.eternalharvest;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,33 +14,52 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Save the default config if it doesn't exist
-        saveDefaultConfig();
+        // Ensure the plugin data folder exists
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdir();
+        }
 
         // Initialize the database connection
+        File databaseFile = new File(getDataFolder(), "plant_growth.db");
+
+        // Check if the database file already exists
+        if (!databaseFile.exists()) {
+            try {
+                if (databaseFile.createNewFile()) {
+                    getLogger().info("Created new database file: " + databaseFile.getName());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                getLogger().severe("Failed to create database file: " + databaseFile.getName());
+                return;
+            }
+        }
+
+        // Connect to the database
         try {
-            // Replace "eternalharvest" with your desired folder name
-            connection = DriverManager.getConnection("jdbc:sqlite:plugins/eternalharvest/plant_growth.db");
+            String jdbcUrl = "jdbc:sqlite:" + databaseFile.getAbsolutePath();
+            connection = DriverManager.getConnection(jdbcUrl);
             getLogger().info("Database connected successfully.");
         } catch (SQLException e) {
             e.printStackTrace();
+            getLogger().severe("Failed to connect to database.");
+            return;
         }
 
         // Register event listeners
         getServer().getPluginManager().registerEvents(new PlantEventListener(this), this);
         getServer().getPluginManager().registerEvents(new ChunkEventListener(this), this);
-        getLogger().info("eternalharvest plugin has been enabled!");
+        getLogger().info("EternalHarvest plugin has been enabled!");
 
         // Initialize PlantGrowthManager
         PlantGrowthManager.getInstance().initialize(this, connection);
-
     }
 
     @Override
     public void onDisable() {
         // Save plant data before shutting down
         PlantGrowthManager.getInstance().saveAllPlantData();
-        getLogger().info("eternalharvest has been disabled!");
+        getLogger().info("EternalHarvest has been disabled!");
 
         // Close the database connection
         try {
